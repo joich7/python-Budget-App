5f1a7eb91a68e27e64401675ccee18253f48caf0
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-03-16 22:35:56 +0000

    got string method working

--- src/Category.py
+++ src/Category.py
@@ -1,18 +1,21 @@
 class category:
     
 
-    def __init__(self):
+    def __init__(self, category):
         self.ledger = []
- 
+        self.deposits = 0
+        self.withdraws = 0
 
-    def deposit(self, amount, description):
+        self.category = category
+
+    def deposit(self, amount, description=""):
         self.ledger.append({"amount":amount, "description":description})
         self.funds = amount
 
     def withdraw(self, amount, description):
        
         if amount <= self.check_funds():
-            amount = - amount
+            amount = -amount
             self.ledger.append({"withdraw": amount, "description": description})
         else:
             print(f"insufficient funds! could not withdraw ${amount} for {description}")
@@ -30,18 +33,26 @@ class category:
         #print(self.funds)
         return(self.funds + sum)#adding because sum is a negative number
 
-    def message(self):
-        outputStr = ""
-        for i in self.ledger:
-            if "withdraw" in i.keys():
-                outputStr += (" "+ str(i["description"])+ str(i["withdraw"]) + " /n")
-        print(outputStr)
+    #transfer function/ will need target input and amount. use check funds
+    def transfer(self, amount, destination):
+        if amount <= self.check_funds():
+            self.ledger.append({"withdraw": -amount, "description": f"transer to {destination}"})
+            destination.ledger.append({"deposit": amount, "description": f"transfer from {self.category} "})
 
+        else:
+            print(f"insufficient funds! could not transfer ${amount} to {destination}")
+
+
+        
     def __str__(self):
-        outputStr = f""""""
-        for i in self.ledger:
-            if "withdraw" in i.keys():
-                outputStr += (str(i["description"])+ str(i["withdraw"]))
+        ledger = self.ledger 
+        outputStr = "Category: " + str(self.category)+"\n"
+        outputStr += f"Initial Deposit:{ledger[0]['amount']}" +"\n"
+        for i in ledger:
+            if "withdraw" in i.keys() or "transfer" in i.keys():
+                outputStr += (str(i["description"]) + str(i["withdraw"]))+"\n"
+        outputStr += "Total: " + str(self.check_funds()) + "\n"
+        
         return(outputStr)
 
   
--- src/main.py
+++ src/main.py
@@ -2,14 +2,24 @@ from Category import category
 
 
 
-food = category()
-
-food.deposit(20, "Depositing this weeks payheck")
+food = category("food")
+food.deposit(2000, "Initial Deposit")
 food.withdraw(10, "Groceries")
 food.withdraw(5, "Mediterannian grill")
 food.withdraw(100, "bacon")
-print(food.ledger) 
-print(food.check_funds())
+
+
+
+
+clothes = category("clothes")
+clothes.deposit(100, "Initial deposit")
+clothes.withdraw(10, "bought new high heels")
+
+
+#print(food.ledger) 
+#print(food.check_funds())
 #print(food.message())
 print(food)
+print(clothes)
 
+food.transfer(40, clothes)
